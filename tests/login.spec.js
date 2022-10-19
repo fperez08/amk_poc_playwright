e102a0d04018b085db0ba68805324af0eb8cdb91
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-10-19 11:02:00 -0500

    Reorganize test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,26 +1,26 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test.use({storageState:'standard_user_state.json'});
-test('Login with a standard user', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/inventory.html');
+test.describe("Standard user", () => {
+    test.use({ storageState: "standard_user_state.json" });
+    test("Successful login ", async ({ page }) => {
+        await page.goto("https://www.saucedemo.com/inventory.html");
 
-  const header = page.locator(".title");
-  await expect(header).toHaveText('Products')
-
+        const header = page.locator(".title");
+        await expect(header).toHaveText("Products");
+    });
 });
 
-test.describe(() => {
-  test.use({storageState:'problem_user_state.json'});
-  test('Login with a problem user', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/inventory.html');
-  
-    const header = page.locator(".title");
-    await expect(header).toHaveText('Products')
+test.describe("Problem user", () => {
+    test.use({ storageState: "problem_user_state.json" });
+    test("Incorrect image displayed", async ({ page }) => {
+        await page.goto("https://www.saucedemo.com/inventory.html");
 
-    const incorrectImage = page.locator("img[alt='Sauce Labs Backpack']");
-    const srcText = await incorrectImage.getAttribute('src');
-    await expect(srcText).toContain('sl-404');
-  
-  });
+        const header = page.locator(".title");
+        await expect(header).toHaveText("Products");
+
+        const incorrectImage = page.locator("img[alt='Sauce Labs Backpack']");
+        const srcText = await incorrectImage.getAttribute("src");
+        await expect(srcText).toContain("sl-404");
+    });
 });
